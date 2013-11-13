c587223f7619b84b393ccc7dc3ab3d2ce6d90276
ruby _v <lien.jennifer@gmail.com>
2013-11-13 02:30:44 -0600

    Updated Image Tag, fixed Show Link on Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rxngif::Application.routes.draw do
-  get("/picture_details/:id", { :controller => "pictures", :action => "show" })
-
-  get("/all_pictures", {:controller => "pictures", :action => "index"})
-
+  
+#CREATE
   get("/new_picture_form", { :controller => "pictures", :action => "new" })
-
   get("/create_picture", { :controller => "pictures", :action => "create" })
 
-  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
-
+#READ
+ get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+ get("/all_pictures", {:controller => "pictures", :action => "index"})
+ 
+#UPDATE 
+  get("/update_picture/:id", { :controller => "pictures", :action => "update" })	
   get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
 
-  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
+#DELETE
+  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+  
 end
