58db1353642d439e9684e26a3ee832782c32846a
ruby _v <lien.jennifer@gmail.com>
2013-11-13 03:24:01 -0600

    FORM tag, Label Tag

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rxngif::Application.routes.draw do
   
 #CREATE
   get("/new_picture_form", { :controller => "pictures", :action => "new" })
-  get("/create_picture", { :controller => "pictures", :action => "create" })
+  post("/create_picture", { :controller => "pictures", :action => "create" })
 
 #READ
  get("/picture_details/:id", { :controller => "pictures", :action => "show" })
  get("/all_pictures", {:controller => "pictures", :action => "index"})
- 
+
 #UPDATE 
-  get("/update_picture/:id", { :controller => "pictures", :action => "update" })	
+  patch("/update_picture/:id", { :controller => "pictures", :action => "update" })	
   get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
 
 #DELETE
-  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+  delete("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
   
 end
